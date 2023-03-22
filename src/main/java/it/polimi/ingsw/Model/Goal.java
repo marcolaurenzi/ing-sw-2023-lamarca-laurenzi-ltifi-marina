14b39585e93c852e850ad327ec017c602845a992
Marco Laurenzi <marco.laurenzi@mail.polimi.it>
2023-03-22 11:27:28 +0100

    First Classes addition
    BookShelf, CommonGoal and a specific CommonGoal classes added but not implemented. Book Interface added. UML slightly modified

---
+++ src/main/java/it/polimi/ingsw/Model/Goal.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.Model;
+
+public interface Goal {
+    public boolean isAchieved(BookShelf bookShelf);
+}
---
+++ src/main/java/it/polimi/ingsw/Model/ThreeColumnsOfSixTilesWithMaxThreeDifferencesCommonGoal.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.Model;
+
+/**
+ * This class represents the CommonGoal number 4
+ * (based on the official Rulebook file starting with number 0 from the top left corner and finishing with number 11 in the bottom right corner)
+ * saying that a player must have, to achieve the goal, 3 different columns
+ * of 6 Tiles with no more than 3 different colours in it
+ */
+
+
+public class ThreeColumnsOfSixTilesWithMaxThreeDifferencesCommonGoal extends CommonGoal{
+
+    /**
+     * isAchieved() method checks weather the CommonGoal is achieved or not in the given BookShelf
+     * @param bookShelf is the reference to the actual BookShelf Object where the Algorithm works on
+     *
+     * @return the method returns true weather the Goals is Achieved and false otherwise
+     */
+
+    @Override
+    public boolean isAchieved(BookShelf bookShelf) {
+        return true;
+    }
+}
