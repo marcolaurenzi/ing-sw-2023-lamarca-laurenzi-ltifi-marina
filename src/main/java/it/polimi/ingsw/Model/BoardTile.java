2e30116f474fd1db5749c4ee57e70b353cef03c1
Alessandro Marina <71550720+Hackingale@users.noreply.github.com>
2023-03-23 12:07:58 +0100

    Update BoardTile.java
    Changed the comments into documentation

--- src/main/java/it/polimi/ingsw/Model/BoardTile.java
+++ src/main/java/it/polimi/ingsw/Model/BoardTile.java
@@ -6,23 +6,23 @@ package it.polimi.ingsw.Model;
  */
 
 public class BoardTile {
-    /* numberOfPlayersSign attribute is used to determine if the tile
+    /** numberOfPlayersSign attribute is used to determine if the tile
     * must be left empty or not based on the numbers of players at the beginning of the game
     */
     private final int numberOfPlayersSign;
     private Item placedItem;
-    /* the constructor is used when the board is initialized and
+    /** the constructor is used when the board is initialized and
     * all the number of each board tile must be assigned
     * the placed item field is set to null as it is empty at the beginning of the game*/
     public BoardTile(int numberOfPlayersSign){
         this.numberOfPlayersSign = numberOfPlayersSign;
         this.placedItem = null;
     }
-    /* this method tells whether the tile is empty or not */
+    /** this method tells whether the tile is empty or not */
     private boolean isEmpty(){
         return placedItem == null;
     }
-    /* this method is used to place an item on the tile if it's empty*/
+    /** this method is used to place an item on the tile if it's empty*/
     private void placeItem(Item itemToPlace){
         if(this.isEmpty()) {
             this.placedItem = itemToPlace;
@@ -30,7 +30,7 @@ public class BoardTile {
             System.out.println("The tile is not empty!");
         }
     }
-    /* this method is used to draw the item from the tile if it's present*/
+    /** this method is used to draw the item from the tile if it's present*/
     private Item drawItem(){
         if(!this.isEmpty()){
             Item temp = this.placedItem;
@@ -41,7 +41,7 @@ public class BoardTile {
             return null;
         }
     }
-    /* this method is used to check which item is placed one the tile
+    /** this method is used to check which item is placed one the tile
     * It is useful in case the game runs in the mode without the GUI
     * Hence this allows the player to check the item without drawing it*/
     private Item checkItem(){
