02a0d6e606cad038cad0b818e8f4f3bf801afd73
mouadhltifi <mouadh.ltifi@hotmail.com>
2023-03-23 13:18:03 +0100

    Changed ColorEnum in TypeEnum with necessary fixes in other classes

--- src/main/java/it/polimi/ingsw/Model/ColorEnum.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.Model;
-
-/**
- * This enum is used to set the color of the items used in the game
- */
-public enum ColorEnum {
-    BLUE, GREEN, YELLOW, WHITE, PINK, LIGHTBLUE
-}
--- src/main/java/it/polimi/ingsw/Model/ElementEnum.java
+++ src/main/java/it/polimi/ingsw/Model/ElementEnum.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.Model;
 
-import static it.polimi.ingsw.Model.ColorEnum.*;
+import static it.polimi.ingsw.Model.TypeEnum.*;
 
 /**
  * This enum is used to set the element type of each item
@@ -9,28 +9,29 @@ import static it.polimi.ingsw.Model.ColorEnum.*;
  */
 
 public enum ElementEnum {
-    FRAMES1(BLUE),
-    FRAMES2(BLUE),
-    FRAMES3(BLUE),
-    CAT1(GREEN),
-    CAT2(GREEN),
-    CAT3(GREEN),
-    TOYS1(YELLOW),
-    TOYS2(YELLOW),
-    TOYS3(YELLOW),
-    BOOKS1(WHITE),
-    BOOKS2(WHITE),
-    BOOKS3(WHITE),
-    PLANT1(PINK),
-    PLANT2(PINK),
-    PLANT3(PINK),
-    AWARDS1(LIGHTBLUE),
-    AWARDS2(LIGHTBLUE),
-    AWARDS3(LIGHTBLUE);
-    private ColorEnum itemColor;
-    public ColorEnum getItemColor(){ return this.itemColor;}
-    private ElementEnum(ColorEnum itemColor){
-        this.itemColor = itemColor;
+    //modifica fatta da discutere
+    CAT1(CATS),
+    CAT2(CATS),
+    CAT3(CATS),
+    BOOK1(BOOKS),
+    BOOK2(BOOKS),
+    BOOK3(BOOKS),
+    GAME1(GAMES),
+    GAME2(GAMES),
+    GAME3(GAMES),
+    FRAME1(FRAMES),
+    FRAME2(FRAMES),
+    FRAME3(FRAMES),
+    TROPHY1(TROPHIES),
+    TROPHY2(TROPHIES),
+    TROPHY3(TROPHIES),
+    PLANT1(PLANTS),
+    PLANT2(PLANTS),
+    PLANT3(PLANTS);
+    private TypeEnum itemType;
+    public TypeEnum getItemType(){ return this.itemType;}
+    private ElementEnum(TypeEnum itemType){
+        this.itemType = itemType;
     }
 
 }
