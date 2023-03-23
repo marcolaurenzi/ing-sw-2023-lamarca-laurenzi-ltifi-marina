35ec36bf7b10940b9ac4b0c70f63a23ee155b0c3
Alessandro Marina <71550720+Hackingale@users.noreply.github.com>
2023-03-23 12:23:47 +0100

    Create Board.java
    Created the Class corresponding to the game board => not finished

---
+++ src/main/java/it/polimi/ingsw/Model/Board.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.Model;
+/**
+ * this class represents the game board of the game
+ */
+public class Board {
+    private BoardTile[][] gameBoard;
+    public Board() {
+        /* to be finished  */
+    }
+
+    private boolean hasfree(int x, int y){
+    /* to be finished */
+    }
+
+}
