93766df209f5870efb09d48c00f94cfc0cb34d39
Alessandro Marina <71550720+Hackingale@users.noreply.github.com>
2023-03-24 18:49:12 +0100

    Update Board.java

--- src/main/java/it/polimi/ingsw/Model/Board.java
+++ src/main/java/it/polimi/ingsw/Model/Board.java
@@ -5,7 +5,10 @@ package it.polimi.ingsw.Model;
 public class Board {
     private BoardTile[][] gameBoard;
     public Board() {
-        /* to be finished  */
+        for(int i = 0; i < 10;i++)
+            for(int j = 0; j < 10;j++){
+                gameBoard[i][j] = new BoardTile(); //capiamo come mettere i numeri nelle singole tile
+            }
     }
 
     private boolean hasfree(int x, int y){
