d486784f72d450283155317dc1971b2f02129ad4
Alessandro Marina <71550720+Hackingale@users.noreply.github.com>
2023-03-24 18:54:36 +0100

    Delete TypeEnum.java

--- src/main/java/it/polimi/ingsw/Model/TypeEnum.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.Model;
-
-/**
- * This enum is used to set the type of the items used in the game
- */
-public enum TypeEnum {
-    //modifica fatta da discutere
-    CATS, BOOKS, GAMES, FRAMES, TROPHIES, PLANTS
-}
