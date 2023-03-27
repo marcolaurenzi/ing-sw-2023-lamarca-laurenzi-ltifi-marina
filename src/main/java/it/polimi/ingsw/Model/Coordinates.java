9bbdc7d406578c969af50b40472380dca190e173
Riccardo250 <riccardo.lamarca01@gmail.com>
2023-03-27 20:54:31 +0200

    Added Coordinates.java

---
+++ src/main/java/it/polimi/ingsw/Model/Coordinates.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.Model;
+
+public class Coordinates {
+    final int x;
+    final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+}
