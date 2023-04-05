3624c410307fa190947166a6e1be7855533c0697
Riccardo250 <riccardo.lamarca01@gmail.com>
2023-04-05 11:17:22 +0200

    Added Exception, created test for Player

--- src/main/java/it/polimi/ingsw/Model/Game.java
+++ src/main/java/it/polimi/ingsw/Model/Game.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.Model;
 
 import it.polimi.ingsw.Model.Decks.*;
+import it.polimi.ingsw.Model.Exceptions.MaxNumberOfPlayersException;
 import it.polimi.ingsw.Model.Exceptions.NumberOfPlayersException;
 import it.polimi.ingsw.Model.GameState.*;
 import it.polimi.ingsw.Model.Goals.CommonGoals.CommonGoal;
@@ -11,17 +12,17 @@ import java.util.ArrayList;
 public class Game {
     private GameState gameState; //to be fixed by moving the methods body into the states and callign gameState.methods in this class
     final String gameID;
-    private Deck itemDeck;
-    private Deck commonGoalDeck;
-    private Deck personalGoalDeck;
+    private ItemDeck itemDeck;
+    private CommonGoalDeck commonGoalDeck;
+    private PersonalGoalDeck personalGoalDeck;
     private CommonGoalPointStack[] commonGoalPointStacks;
     final int maxPlayers;
     private int currentPlayer;
     private ArrayList<Player> players;
-    private Board board;
-    public Game(String gameId, int maxPlayers) throws NumberOfPlayersException, IOException {
+    private final Board board;
+    public Game(String gameId, int maxPlayers) throws MaxNumberOfPlayersException, IOException {
         if(maxPlayers > 4 || maxPlayers < 2) {
-            throw new NumberOfPlayersException();
+            throw new MaxNumberOfPlayersException();
         }
         gameState = new GameStateStarting();
         this.gameID = gameId;
@@ -30,7 +31,6 @@ public class Game {
         board = Utils.loadBoardFromFile("src/main/resources/configurations/BoardConfiguration.JSON");
         this.initializeDeck();
     }
-
 
     /**
      * This method is called when a player joins the game and adds him to the list of players
@@ -76,19 +76,19 @@ public class Game {
      * @throws IOException
      * @throws NumberOfPlayersException
      */
-    public void initializeDeck() throws IOException, NumberOfPlayersException {
+    public void initializeDeck() throws IOException {
 
         (itemDeck = new ItemDeck()).initializeDeck();
         (commonGoalDeck = new CommonGoalDeck()).initializeDeck();
         (personalGoalDeck = new PersonalGoalDeck()).initializeDeck();
 
         commonGoalPointStacks = new CommonGoalPointStack[2];
-        commonGoalPointStacks[0] = new CommonGoalPointStack((CommonGoal) commonGoalDeck.draw(), maxPlayers); //TODO cast to be solved
-        commonGoalPointStacks[1] = new CommonGoalPointStack((CommonGoal) commonGoalDeck.draw(), maxPlayers); //TODO cast to be solved
+        commonGoalPointStacks[0] = new CommonGoalPointStack(commonGoalDeck.draw(), maxPlayers); //TODO cast to be solved
+        commonGoalPointStacks[1] = new CommonGoalPointStack(commonGoalDeck.draw(), maxPlayers); //TODO cast to be solved
     }
 
     public void startGame() throws IOException, NumberOfPlayersException {
-        gameState.startGame(board, (ItemDeck) itemDeck, maxPlayers);
+        gameState.startGame(board, itemDeck, maxPlayers);
         this.gameState = new GameStateRunning();
     }
 
---
+++ src/test/java/it/polimi/ingsw/Model/GameTest.java
@@ -0,0 +1,76 @@
+package it.polimi.ingsw.Model;
+
+import it.polimi.ingsw.Model.Exceptions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void constructorThrowsNumberOfPlayerException() {
+        assertThrows(MaxNumberOfPlayersException.class, () -> new Game("3", 5));
+    }
+
+    @Test
+    void constructorThrowsNoException() {
+        assertDoesNotThrow(() -> new Game("3", 3));
+    }
+
+    @Test
+    void addFirstPlayer() throws MaxNumberOfPlayersException, IOException, NumberOfPlayersException {
+        Game game = new Game("1", 4);
+
+        String playerID = "54336";
+        game.addPlayer(playerID);
+        assertEquals(1, game.getPlayers().size());
+        assertEquals(playerID, game.getPlayers().get(0).getPlayerID());
+    }
+
+    @Test
+    void testAddPlayer() {
+    }
+
+    @Test
+    void initializeBoard() {
+    }
+
+    @Test
+    void nextPlayer() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void getMaxPlayers() {
+    }
+
+    @Test
+    void getGameID() {
+    }
+
+    @Test
+    void getPlayers() {
+    }
+
+    @Test
+    void initializeDeck() {
+    }
+
+    @Test
+    void startGame() {
+    }
+
+    @Test
+    void getPersonalGoalDeck() {
+    }
+
+    @Test
+    void getCommonGoalPointStacks() {
+    }
+}
