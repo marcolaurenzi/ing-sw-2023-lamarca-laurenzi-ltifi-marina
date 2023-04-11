aebaba5da63f51000b53c09860a098e33558c3e1
Marco Laurenzi <marco.laurenzi@mail.polimi.it>
2023-04-11 12:52:02 +0200

    CommonGoal10 Test completed

--- src/main/java/it/polimi/ingsw/Model/Goals/CommonGoals/CommonGoal10.java
+++ src/main/java/it/polimi/ingsw/Model/Goals/CommonGoals/CommonGoal10.java
@@ -1,6 +1,11 @@
 package it.polimi.ingsw.Model.Goals.CommonGoals;
 
 import it.polimi.ingsw.Model.Bookshelf;
+import it.polimi.ingsw.Model.Matrix;
+import it.polimi.ingsw.Model.TypeEnum;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -16,22 +21,65 @@ public class CommonGoal10 extends CommonGoal{
     @Override
     public boolean isAchieved(Bookshelf bookshelf) {
 
-        for (int i = 0; i < bookshelf.getColumnDimension() - 2; i++) {
-            for (int j = 0; j < bookshelf.getColumnDimension() - 2; j++) {
-                if (bookshelf.get(i, j) == null ||
-                        bookshelf.get(i + 2, j) == null ||
-                        bookshelf.get(i + 1, j + 1) == null ||
-                        bookshelf.get(i + 2, j) == null ||
-                        bookshelf.get(i + 2, j + 2) == null) {
-                    break; // if there is a null, there is no need to check the other cells
-                } else if (bookshelf.get(i, j).getType() == bookshelf.get(i + 2, j).getType() &&
-                        bookshelf.get(i, j).getType() == bookshelf.get(i + 1, j + 1).getType() &&
-                        bookshelf.get(i, j).getType() == bookshelf.get(i + 2, j).getType() &&
-                        bookshelf.get(i, j).getType() == bookshelf.get(i + 2, j + 2).getType()) {
-                    return true;
-                } // if all the cells have the same type, the goal is achieved
+        Set<TypeEnum> temp = new HashSet<>();
+        int count = 0;
+        TypeEnum currTypeEnum;
+
+        for(int i = 0; i < bookshelf.getColumnDimension()-2; i++) {
+            for(int j = 0; j < bookshelf.getRowDimension()-2; j++) {
+                if(bookshelf.get(i,j) != null && bookshelf.get(i+2,j) != null &&
+                        bookshelf.get(i+1,j+1) != null && bookshelf.get(i,j+2) != null &&
+                        bookshelf.get(i+2,j+2) != null) {
+                    currTypeEnum = bookshelf.get(i,j).getType();
+                    if(bookshelf.get(i,j).getType() == bookshelf.get(i+2,j).getType() &&
+                            bookshelf.get(i+2,j).getType() == bookshelf.get(i+2,j+2).getType() &&
+                            bookshelf.get(i+2,j+2).getType() == bookshelf.get(i,j+2).getType() &&
+                            bookshelf.get(i,j+2).getType() == bookshelf.get(i+1,j+1).getType()){
+
+                        if(bookshelf.get(i+1,j) == null) {
+                            count++;
+                        }
+                        else {
+                            if(bookshelf.get(i+1,j).getType() != currTypeEnum) {
+                                temp.add(bookshelf.get(i+1,j).getType());
+                            }
+                        }
+                        if(bookshelf.get(i,j+1) == null) {
+                            count++;
+                        }
+                        else {
+                            if(bookshelf.get(i,j+2).getType() != currTypeEnum) {
+                                temp.add(bookshelf.get(i,j+1).getType());
+                            }
+                        }
+                        if(bookshelf.get(i+2,j+1) == null) {
+                            count++;
+                        }
+                        else {
+                            if(bookshelf.get(i+2,j+1).getType() != currTypeEnum) {
+                                temp.add(bookshelf.get(i+2,j+1).getType());
+                            }
+                        }
+                        if(bookshelf.get(i+1,j+2) == null) {
+                            count++;
+                        }
+                        else {
+                            if(bookshelf.get(i+1,j+2).getType() != currTypeEnum) {
+                                temp.add(bookshelf.get(i+1,j+2).getType());
+                            }
+                        }
+
+                        if(count + temp.size() == 4) {
+                            return true;
+                        }
+                        count=0;
+                        temp.clear();
+                    }
+                }
+
             }
         }
         return false;
+
     }
 }
---
+++ src/test/java/it/polimi/ingsw/Model/Goals/CommonGoals/CommonGoal10Test.java
@@ -0,0 +1,58 @@
+package it.polimi.ingsw.Model.Goals.CommonGoals;
+
+import it.polimi.ingsw.Model.Bookshelf;
+import it.polimi.ingsw.Model.Utils;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class CommonGoal10Test {
+    final CommonGoal goal = new CommonGoal10();
+
+    /**
+     * Testing the corner case where the pointer is null
+     *
+     * @throws IOException
+     */
+    @Test
+    public void nullPointerTest() throws IOException {
+        Bookshelf bookshelf = Utils.loadBookshelfFromFile(Utils.getTestFilesPath() + "/nullBookShelfTest.JSON");
+        assertThrows(NullPointerException.class, () -> goal.isAchieved(bookshelf));
+    }
+
+    /**
+     * Testing corner case where the whole matrix is void
+     *
+     * @throws IOException
+     */
+    @Test
+    public void nullBookshelfTest() throws IOException {
+        Bookshelf bookshelf = Utils.loadBookshelfFromFile(Utils.getTestFilesPath() + "/commonGoal10Test.JSON", 0);
+        assertEquals(false, goal.isAchieved(bookshelf));
+    }
+
+    /**
+     * Testing the corner case where the matrix has only one X, the minimum allowed value
+     *
+     * @throws IOException
+     */
+    @Test
+    public void oneXBookshelfTest() throws IOException {
+        Bookshelf bookshelf = Utils.loadBookshelfFromFile(Utils.getTestFilesPath() + "/commonGoal10Test.JSON", 1);
+        assertEquals(true, goal.isAchieved(bookshelf));
+    }
+
+    /**
+     * Testing the corner case where the matrix has an invalid X, an X that's not isolated
+     *
+     * @throws IOException
+     */
+    @Test
+    public void zeroXBookshelfTest() throws IOException {
+        Bookshelf bookshelf = Utils.loadBookshelfFromFile(Utils.getTestFilesPath() + "/commonGoal10Test.JSON", 2);
+        assertEquals(false, goal.isAchieved(bookshelf));
+    }
+}
