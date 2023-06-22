b10c89382b28eb982f695ae334922cbcc9c65899
Marco Laurenzi <marco.laurenzi@mail.polimi.it>
2023-06-22 20:03:27 +0200

    UI selection implemented

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -10,7 +10,9 @@ import it.polimi.ingsw.View.LoginController;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.rmi.NotBoundException;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -19,11 +21,24 @@ import java.rmi.NotBoundException;
 public class App 
 {
     public static void main( String[] args ) throws WrongMessageClassEnumException, NotBoundException, MaxNumberOfPlayersException, AlreadyStartedGameException, IOException, InterruptedException {
-        // GUI
-        LoginController.main(args);
 
-        // TUI
-        // TUI tui = new TUI();
-        // tui.run();
+        System.out.println("Welcome to MyShelfie! PLease select the UI you want to use.");
+        System.out.println("TUI - GUI");
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        String ui = scanner.nextLine();
+        boolean started = false;
+        while (!started) {
+            if (ui.equals("TUI") || ui.equals("tui") || ui.equals("Tui")) {
+                TUI tui = new TUI();
+                tui.run();
+                started = true;
+            } else if (ui.equals("GUI") || ui.equals("gui") || ui.equals("Gui")) {
+                LoginController.main(args);
+                started = true;
+            } else {
+                System.out.println("Invalid input. Please try again.");
+                ui = scanner.nextLine();
+            }
+        }
     }
 }
